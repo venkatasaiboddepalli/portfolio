3d6efa25cb1f5a814b32ad688a72111976387960
venkatasaiboddepalli <boddepallivenkatsai2005@gmail.com>
2026-03-20 00:09:38 +0530

    remove router fix github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -12,11 +11,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/portfolio/">
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </HashRouter>
+      <Index />
     </TooltipProvider>
   </QueryClientProvider>
 );
